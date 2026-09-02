dca957efd878a65c196229d8cb232206b35488ae
mauriciobetan <35309011+mauriciobetan@users.noreply.github.com>
2026-09-02 16:12:14 -0400

    Update check_packages.R

--- check_packages.R
+++ check_packages.R
@@ -5,8 +5,7 @@
 #http://www.vikram-baliga.com/blog/2015/7/19/a-hassle-free-way-to-verify-that-r-packages-are-installed-and-loaded
 
 #add new packages to the chain here
-packages = c("foreign","texreg","stargazer","survey","psych", "readxl","tidyr", "ggplot2", "panelAR", "gtable",
-             "grid", "gridExtra")
+packages = c("readxl","dplyr","ggplot2","scales","lubridate")
 
 package.check <- lapply(packages, FUN = function(x) {
   if (!require(x, character.only = TRUE)) {
