505f3b868323632d802c0f36e58d04cf784e6d47
mauriciobetan <35309011+mauriciobetan@users.noreply.github.com>
2026-09-21 12:55:34 -0400

    Update check_packages.R

--- check_packages.R
+++ check_packages.R
@@ -5,7 +5,7 @@
 #http://www.vikram-baliga.com/blog/2015/7/19/a-hassle-free-way-to-verify-that-r-packages-are-installed-and-loaded
 
 #add new packages to the chain here
-packages = c("readxl","dplyr","ggplot2","scales","lubridate")
+packages = c("readxl","dplyr","ggplot2","scales","tidyr","lubridate")
 
 package.check <- lapply(packages, FUN = function(x) {
   if (!require(x, character.only = TRUE)) {
